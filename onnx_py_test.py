df7b58186959b5bbbe584d2341a44452686af4ce
Sajjad Aemmi <sajjadaemmi@gmail.com>
2023-07-25 13:16:54 +0330

    Fix bugs

--- onnx_py_test.py
+++ onnx_py_test.py
@@ -1,37 +1,21 @@
+import time
 import onnxruntime as ort
-import numpy as np
 import numpy as np
 import cv2
 
-img=cv2.imread('fr.jpg')
-img=cv2.resize(img,(640,640))
-img=img.transpose(2,0,1)
-img=img.reshape(1,3,640,640)
-img=img.astype(np.float32)
+img = cv2.imread('fr.jpg')
+img = cv2.resize(img, (640, 640))
+img = img.transpose(2, 0, 1)
+img = img.reshape(1, 3, 640, 640)
+img = img.astype(np.float32)
+img = img / 255.0
 
 
-modelInputShape = [1, 3, 640, 640]
-topk = 1
-iouThreshold = 0.45
-scoreThreshold = 0.25
+ort_sess = ort.InferenceSession('public/model/yolov8n-pose.onnx', providers=['CPUExecutionProvider'])
 
 
-config = {
-    "top_k": topk,
-    "iou_threshold": iouThreshold,
-    "score_threshold": scoreThreshold,
-}
-
-
-# Load Model
-ort_sess = ort.InferenceSession('yolov8n-pose.onnx')
-#load nms model
-ort_sess_nms = ort.InferenceSession('modified_nms-yolov8.onnx')
-# Run inference
-
-outputs = ort_sess.run(None,{'images': img})
-selected_indices = ort_sess_nms.run(None,{ "detection": outputs ,"config": config })
-
-
-print(outputs[0].shape)
-print(selected_indices[0])
+while True:
+    start = time.time()
+    outputs = ort_sess.run(None, {'images': img})
+    end = time.time()
+    print('time: ', end - start)
